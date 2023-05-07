8d6715dce0c8f64c4ff7fa018321e33aec03f0cf
2436770147@qq.com <2436770147@qq.com>
2023-05-07 21:42:02 +0800

    第27次提交

--- src/main/java/com/test/Test0405.java
+++ src/main/java/com/test/Test0405.java
@@ -7,6 +7,14 @@ public class Test0405 {
         StringBuffer b=new StringBuffer("b");
         function(a,b);
         System.out.println(a+" "+b);
+
+        System.out.println();
+        System.out.println();
+
+        //Integer值在-128~127之间是值比较
+        Integer i=-128;
+        Integer j=-128;
+        System.out.println(i==j);
     }
 
     private static void function(StringBuffer x, StringBuffer y) {
