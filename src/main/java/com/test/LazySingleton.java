6e2705412f2e70ecde15f95461944646cdbba7ff
2436770147@qq.com <2436770147@qq.com>
2023-05-11 10:36:34 +0800

    第42次提交

--- src/main/java/com/test/LazySingleton.java
+++ src/main/java/com/test/LazySingleton.java
@@ -5,35 +5,35 @@ import java.util.concurrent.*;
 //懒加载单例+线程安全
 public class LazySingleton {
 
-    private static LazySingleton INSTANCE = null;
+    private static LazySingleton instance = null;
 
     private LazySingleton(){
 
     }
 
     public static LazySingleton getInstance(){
-//        //synchronized同步代码块+双重检查锁实现线程安全
-//        //避免第一次重复new对象
-//        if(INSTANCE==null){
-//            try {
-//                Thread.sleep(500);
-//            } catch (InterruptedException e) {
-//                e.printStackTrace();
-//            }
-//            synchronized (LazySingleton.class){
-//                //避免线程抢锁重复new对象
-//                if(INSTANCE==null){
-//                    INSTANCE=new LazySingleton();
-//                }
-//            }
-//        }
-//        return INSTANCE;
+        //synchronized同步代码块+双重检查锁实现线程安全
+        //避免第一次重复new对象
+        if(instance==null){
+            try {
+                Thread.sleep(100);
+                synchronized (LazySingleton.class){
+                    //避免线程抢锁重复new对象
+                    if(instance==null){
+                        instance=new LazySingleton();
+                    }
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        return instance;
 
         //使用静态内部类实现线程安全
-        return Inner.INSTANCE;
+//        return Inner.instance;
     }
 
     private static class Inner{
-        private static LazySingleton INSTANCE = new LazySingleton();
+        private static LazySingleton instance = new LazySingleton();
     }
 }
